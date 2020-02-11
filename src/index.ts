163c42af4f793c89585f5b9d1d55ed40a8a5928d
Misha Marinenko <0x77dev@protonmail.com>
2020-02-11 22:07:11 +0200

    That will be hard...

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.info("Empty here... :(");
+import zmq, { Publisher } from 'zeromq';
+import mdns from 'mdns';
+const ad = mdns.createAdvertisement(mdns.tcp('neoxa_pub'), 7878);
+
+(async () => {
+    const sock: Publisher = new zmq.Publisher
+
+    await sock.bind("tcp://0.0.0.0:7878")
+
+    while (true) {
+        console.log("sending a multipart message envelope")
+        await sock.send(["hello", "world"])
+        await new Promise(resolve => setTimeout(resolve, 500))
+    }
+})()
